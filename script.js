283ac2c406c941069e541508d4c53e39a5f61542
Arquimidio <gabrielstoppadossantos@gmail.com>
2021-08-24 18:09:51 -0300

    Drum kit

---
+++ script.js
@@ -0,0 +1,15 @@
+let listaTeclas = document.getElementsByClassName('tecla')
+
+document.addEventListener('keypress', keydownEffect)
+document.addEventListener('keyup', keyUpClearEffect)
+
+function keydownEffect(e){
+    let key = document.getElementById(e.key.toLowerCase())
+    if (key) key.className = "tecla playing"
+    new Audio(`${e.key.toLowerCase()}.wav`).play()
+}
+
+function keyUpClearEffect(e){
+    let key = document.getElementById(e.key.toLowerCase())
+    if (key) key.className = "tecla"
+}
